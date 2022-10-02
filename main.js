d30d200ddf8ed1edb8c85c4358744058b0bcb897
Mirjam <hingeke@gmail.com>
2022-10-02 15:23:02 +0300

    L2

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    // data: function() 
+    //ES6 shorthand of data: function()
+        data() {
+        return {
+            product: 'Sokid',
+            productDescription: 'Need on väga head'
+        }
+    }
+})
+
+
