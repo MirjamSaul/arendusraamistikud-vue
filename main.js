033c6b4a759e4d262a7072435f6c662fd469f849
Mirjam <hingeke@gmail.com>
2022-10-03 23:46:35 +0300

    L5-listrendering

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ const app = Vue.createApp({
             product: 'Sokid',
             image: './assets/images/socks_blue.jpg',
             productDescription: 'Need on väga head',
-            inStock: false,
+            //inStock: false,
             inventory: 8,
             onSale: true,
+            details: ['50% puuvill', '30% vill', '20% mohäär'],
+            variants: [
+                { id:1112, color: 'roheline' },
+                { id:1113, color: 'sinine' },
+            ],
+            sizes: ['37-39', '40-42', '43-45'],
             url:'https://sokid.ee/'
 
         }
