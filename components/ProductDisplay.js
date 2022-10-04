ecf2d3228e5bf39b1b073ec9dfbe7b912c422d33
Mirjam <hingeke@gmail.com>
2022-10-04 15:02:18 +0300

    L9

---
+++ components/ProductDisplay.js
@@ -0,0 +1,102 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean, 
+            required: true
+        }
+    },
+    template: 
+    /*html*/
+    `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!--attribute binding-->
+        <img v-bind:src="image" :class="{ 'out-of-stock-img': !inStock }">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="onSale">{{ saleMessage }}</p>
+        
+        <p>Suurused</p>
+
+        <ul>
+          <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+
+        <product-details :details="details"></product-details>
+        <p>Värvid</p>
+        <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)" class="color-circle" :style="{ backgroundColor: variant.color }">
+        </div>
+
+        <p v-show="onSale">Soodusmüük!</p>
+        
+        <p v-if="inStock">Laos</p>
+
+        <p>Tarne: {{ shipping }}</p>
+        <!--<p v-else-if="variants.quantity <= 10 variants.quantity > 0">Peaaegu otsas</p>-->
+        <p v-else>Otsas</p>
+
+        <a v-bind:href="url">Sokid</a>
+      </div>
+      <div>
+        <p>Tootekirjeldus: {{ productDescription }}</p>
+      </div>
+      <button class="button" :disabled="!inStock" :class="{ disabledButton: !inStock }" @click="addToCart">Osta</button>
+      <button class="removeButton" @click="removeFromCart">Eemalda</button>
+    </div>
+  </div>`,
+  data() {
+    return {
+        product: 'Sokid',
+        brand: 'Heajuust',
+        //image: './assets/images/socks_blue.jpg',
+        selectedVariant: 0,
+        details: ['50% puuvill', '50% mohäär'],
+        productDescription: 'Need on väga head',
+        //inStock: true,
+        //inventory: 0,
+        onSale: true,
+        variants: [
+            { id:1112, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id:1113, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+        ],
+        sizes: ['37-39', '40-42', '43-45'],
+        url:'https://sokid.ee/'
+
+    }
+},
+methods: {
+    addToCart() {
+        this.cart += 1
+    },
+    updateVariant(index) {
+        this.selectedVariant = index
+        
+    },
+    removeFromCart() {
+        this.cart -=1
+    }
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product
+    },
+    image() {
+        return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].quantity
+    },
+    saleMessage() {
+        if (this.onSale) {
+            return this.brand + ' ' + this.product + ' on soodushinnaga!';
+        }
+    },
+    shipping() {
+        if (this.premium) {
+            return 'Tasuta'
+        }
+        return '4.99 EUR'
+    }
+}
+})
--- main.js
+++ main.js
@@ -4,52 +4,10 @@ const app = Vue.createApp({
         data() {
         return {
             cart: 0,
-            product: 'Sokid',
-            brand: 'Heajuust',
-            //image: './assets/images/socks_blue.jpg',
-            selectedVariant: 0,
-            productDescription: 'Need on väga head',
-            //inStock: true,
-            //inventory: 0,
-            onSale: true,
-            details: ['50% puuvill', '30% vill', '20% mohäär'],
-            variants: [
-                { id:1112, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id:1113, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            sizes: ['37-39', '40-42', '43-45'],
-            url:'https://sokid.ee/'
-
+            premium: true
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-            
-        },
-        removeFromCart() {
-            this.cart -=1
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        saleMessage() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' on soodushinnaga!';
-            }
-        }
-    }
+    methods: {}
 })
 
 
