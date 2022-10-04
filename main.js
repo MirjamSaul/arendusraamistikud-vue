b0aa05c3aa5f526f77d968e0c1705e2861abf146
Mirjam <hingeke@gmail.com>
2022-10-04 15:47:10 +0300

    L10

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const app = Vue.createApp({
     //ES6 shorthand of data: function()
         data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                this.cart.splice(index, 1)
+            }
+        }
+    }
 })
 
 
