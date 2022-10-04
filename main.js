a57601e1b1f86f9567c0a4de1c2ad279faf07f25
Mirjam <hingeke@gmail.com>
2022-10-04 13:18:22 +0300

    L8

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Sokid',
-            image: './assets/images/socks_blue.jpg',
+            brand: 'Heajuust',
+            //image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
             productDescription: 'Need on väga head',
-            inStock: false,
-            inventory: 0,
+            //inStock: true,
+            //inventory: 0,
             onSale: true,
             details: ['50% puuvill', '30% vill', '20% mohäär'],
             variants: [
-                { id:1112, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id:1113, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id:1112, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id:1113, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
             sizes: ['37-39', '40-42', '43-45'],
             url:'https://sokid.ee/'
@@ -24,11 +26,28 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            
         },
         removeFromCart() {
             this.cart -=1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' on soodushinnaga!';
+            }
         }
     }
 })
